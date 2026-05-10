2cc5fcd62388e9cf880ca03f1ce1908524991b35
Akinyemi Sodiq Adeyinka <akinyemi07@gmail.com>
2026-05-10 20:27:40 +0100

    finishing example one

---
+++ hello.c
@@ -0,0 +1,10 @@
+//the first program in the world of c 
+//example - write a program that prints hello world when it runs
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello World!\n ");
+	return 0;
+
+}
